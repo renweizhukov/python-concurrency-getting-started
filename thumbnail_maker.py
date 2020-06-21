d80d9e502c377b5ed40114272742469d3ca1459d
Wei Ren <wrn@amazon.com>
2020-06-21 01:57:05 -0700

    Clean up unused ThumbnailMakerService methods and attributes
    Signed-off-by: Wei Ren <wrn@amazon.com>

--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -28,68 +28,11 @@ class ThumbnailMakerService(object):
                 # download each image and save to the input dir
                 img_filename = urlparse(url).path.split('/')[-1]
                 urlretrieve(url, self.input_dir + os.path.sep + img_filename)
-                # self.img_queue.put(img_filename)
                 self.img_list.append(img_filename)
 
                 dl_queue.task_done()
             except Queue.Empty:
                 logging.info('Queue empty')
-
-    def download_images(self, img_url_list):
-        # validate inputs
-        if not img_url_list:
-            return
-        os.makedirs(self.input_dir, exist_ok=True)
-
-        logging.info("beginning image downloads")
-
-        start = time.perf_counter()
-        for url in img_url_list:
-            # download each image and save to the input dir
-            img_filename = urlparse(url).path.split('/')[-1]
-            urlretrieve(url, self.input_dir + os.path.sep + img_filename)
-            self.img_queue.put(img_filename)
-        end = time.perf_counter()
-
-        self.img_queue.put(None)
-        logging.info("downloaded {} images in {} seconds".format(len(img_url_list), end - start))
-
-    def perform_resizing(self):
-        # validate inputs
-        os.makedirs(self.output_dir, exist_ok=True)
-
-        logging.info("beginning image resizing")
-        target_sizes = [32, 64, 200]
-        num_images = len(os.listdir(self.input_dir))
-
-        start = time.perf_counter()
-        while True:
-            filename = self.img_queue.get()
-            if filename:
-                logging.info("resizing image {}".format(filename))
-                orig_img = Image.open(self.input_dir + os.path.sep + filename)
-                for basewidth in target_sizes:
-                    img = orig_img
-                    # calculate target height of the resized image to maintain the aspect ratio
-                    wpercent = (basewidth / float(img.size[0]))
-                    hsize = int((float(img.size[1]) * float(wpercent)))
-                    # perform resizing
-                    img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
-
-                    # save the resized image to the output dir with a modified file name
-                    new_filename = os.path.splitext(filename)[0] + \
-                        '_' + str(basewidth) + os.path.splitext(filename)[1]
-                    img.save(self.output_dir + os.path.sep + new_filename)
-
-                os.remove(self.input_dir + os.path.sep + filename)
-                logging.info("done resizing image {}".format(filename))
-                self.img_queue.task_done()
-            else:
-                self.img_queue.task_done()
-                break
-        end = time.perf_counter()
-
-        logging.info("created {} thumbnails in {} seconds".format(num_images, end - start))
 
     def resize_image(self, filename):
         logging.info("resizing image {}".format(filename))
